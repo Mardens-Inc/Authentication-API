6e034ebbc1e4093b32fd22704f20df23b23ba96e
drew-chase <dcmanproductions@gmail.com>
2024-01-17 15:41:19 -0500

    Update password field length in users table

--- inc/Authentication.inc.php
+++ inc/Authentication.inc.php
@@ -12,7 +12,7 @@ class Authentication
         $sql = "CREATE TABLE IF NOT EXISTS users (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL
+            password VARCHAR(1024) NOT NULL
         )";
         $this->conn->query($sql);
     }
