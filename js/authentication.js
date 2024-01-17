3a6380171c125aaa480cbc4037fd8636e6b0c4d9
drew-chase <dcmanproductions@gmail.com>
2024-01-17 14:59:38 -0500

    Add Authentication class for interacting with the authentication API

---
+++ js/authentication.js
@@ -0,0 +1,73 @@
+/**
+ * Authentication class for interacting with the authentication API.
+ * @class
+ */
+export default class Authentication {
+    /**
+     * Create a new Authentication instance.
+     * @param {boolean} debug - If true, use the local API URL. Otherwise, use the production API URL.
+     */
+    constructor(debug = false) {
+        // Set the API URL based on the debug parameter
+        this.apiUrl = debug ? "http://auth.local/" : "https://auth.mardens.com/";
+    }
+
+    /**
+     * Login with a username and password.
+     * @param {string} username - The username to login with.
+     * @param {string} password - The password to login with.
+     * @return {JSON|boolean} The server's response as a JSON object, or false if the login failed.
+     */
+    async login(username, password) {
+        // Send a POST request to the API with the username and password
+        return await $.ajax({
+            url: this.apiUrl,
+            method: "POST",
+            dataType: "json",
+            data: { username, password },
+            success: (data) => {
+                // Return the data from the server on successful request
+                return data;
+            },
+            error: (err) => {
+                // Return false on error
+                return false;
+            },
+        });
+    }
+
+    /**
+     * Login with a token.
+     * @param {string} token - The token to login with.
+     * @return {JSON|boolean} The server's response as a JSON object, or false if the login failed.
+     */
+    async loginWithToken(token) {
+        // Send a POST request to the API with the token in the Authorization header
+        return await $.ajax({
+            url: this.apiUrl,
+            method: "POST",
+            dataType: "json",
+            headers: { Authorization: token },
+            success: (data) => {
+                // Return the data from the server on successful request
+                return data;
+            },
+            error: (err) => {
+                // Return false on error
+                return false;
+            },
+        });
+    }
+
+    /**
+     * Login with a token stored in a cookie.
+     * @return {JSON|boolean} The server's response, or false if no token was found.
+     */
+    async loginWithTokenFromCookie() {
+        const token = document.cookie.filter((cookie) => {
+            return cookie.includes("token");
+        });
+        if(token.length === 0) return false;
+        return await this.loginWithToken(token);
+    }
+}
