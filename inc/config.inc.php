420005cc22e91136064280423535a15daf53ef0e
drew-chase <dcmanproductions@gmail.com>
2024-02-01 13:31:59 -0500

    Refactor db-config.json file path retrieval
    Changed the method of retrieving the database configuration file path from using SERVER['DOCUMENT_ROOT'] to using __DIR__. This ensures that the path to db-config.json is always resolved correctly, regardless of the server configuration.

--- inc/config.inc.php
+++ inc/config.inc.php
@@ -1,9 +1,9 @@
 <?php
-
-$json = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/db-config.json"), true);
+$path = realpath(__DIR__ . "/../db-config.json");
+$json = json_decode(file_get_contents($path), true);
 
 // Database configuration
-$DB_HOST     = $json["host"];
-$DB_USER     = $json["user"];
+$DB_HOST = $json["host"];
+$DB_USER = $json["user"];
 $DB_PASSWORD = $json["password"];
-$DB_NAME     = $json["database"];
+$DB_NAME = $json["database"];
