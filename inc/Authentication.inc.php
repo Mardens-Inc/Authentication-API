575c66e2bb8d58fdc6a0bea4778b166e3b650af1
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-03-12 08:55:44 -0400

    Update user authentication process
    The user authentication process has been expanded to include additional information, such as IP address and user agent. Also, admin status is now tracked for user profiles. A new token generation and validation process is used to improve security. User activity like the last online time and corresponding IP and user agent is also recorded.

--- inc/Authentication.inc.php
+++ inc/Authentication.inc.php
@@ -14,7 +14,12 @@ class Authentication
         $sql = "CREATE TABLE IF NOT EXISTS users (
             id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(255) NOT NULL,
-            password VARCHAR(1024) NOT NULL
+            password TEXT NOT NULL,
+            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            last_online TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+            last_ip VARCHAR(255),
+            last_user_agent VARCHAR(255),
+            admin BOOLEAN DEFAULT FALSE
         )";
         $this->conn->query($sql);
     }
@@ -34,32 +39,47 @@ class Authentication
             try {
                 // Bind to Active Directory using the provided username and password
                 $ldapBind = @ldap_bind($ldapConn, $ldapUsername, $password);
+                // Authentication successful
+                ldap_unbind($ldapConn);
             } catch (Exception $e) {
                 $ldapBind = false;
             }
 
-            if ($ldapBind) {
-                // Authentication successful
-                ldap_unbind($ldapConn);
-                // $password = password_hash($password, PASSWORD_DEFAULT);
-                $password = hash('sha256', $password);
+            // $password = password_hash($password, PASSWORD_DEFAULT);
+            $password = hash('sha256', $password);
 
-                // Check if user exists in database
-                if (!self::checkIfUserExistsInDatabase($username)) {
-                    // User does not exist, create user and generate token
-                    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-                    $stmt = $this->conn->prepare($sql);
-                    $stmt->bind_param("ss", $username, $password);
-                    $stmt->execute();
-                    $stmt->close();
-                }
+            $ip = $_SERVER['REMOTE_ADDR'];
+            $user_agent = $_SERVER['HTTP_USER_AGENT'];
+            $admin = $ldapBind ? 1 : 0;
 
-                return self::generateToken($username, $password, $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT']);
+            // Check if user exists in database
+            if (!self::checkIfUserExistsInDatabase($username)) {
+                // User does not exist, create user and generate token
+                $sql = "INSERT INTO users (username, password, last_ip, last_user_agent, admin) VALUES (?, ?, ?, ?, ?)";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bind_param("ssssi", $username, $password, $ip, $user_agent, $admin);
+                $stmt->execute();
+                $stmt->close();
             } else {
-                // Authentication failed
-                ldap_unbind($ldapConn);
-                return false;
+                // validate user
+                $sql = "SELECT * FROM users WHERE username = ?";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bind_param("s", $username);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $stmt->close();
+                $row = $result->fetch_assoc();
+                if ($row['password'] !== $password) {
+                    return false;
+                }
+                // update last_online, last_ip, last_user_agent
+                $sql = "UPDATE users SET last_online = CURRENT_TIMESTAMP, last_ip = ?, last_user_agent = ? WHERE username = ?";
+                $stmt = $this->conn->prepare($sql);
+                $stmt->bind_param("sss", $ip, $user_agent, $username);
+                $stmt->execute();
             }
+
+            return self::generateToken($username, $password, $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], $admin);
         } else {
             // Unable to connect to Active Directory
             return false;
@@ -69,27 +89,40 @@ class Authentication
 
     public function loginWithToken(string $token): bool
     {
-        // decode uri component
-        $token = urldecode($token);
-        $decoded_token = json_decode(base64_decode($token), true);
-        if (!isset($decoded_token['username'])) {
-            return false;
-        }
-        $username = $decoded_token['username'];
-        $sql = "SELECT * FROM users WHERE username = ?";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $stmt->close();
-        if ($result->num_rows > 0) {
-            $row = $result->fetch_assoc();
-            $password = $row['password'];
-            $ip = $_SERVER['REMOTE_ADDR'];
-            $user_agent = $_SERVER['HTTP_USER_AGENT'];
-            if (self::validate($username, $password, $ip, $user_agent, $token)) {
-                return true;
+
+        try {
+
+            // decode uri component
+            $token = urldecode($token);
+            $decoded_token = json_decode(base64_decode($token), true);
+            if (!isset($decoded_token['username'])) {
+                return false;
             }
+            $username = $decoded_token['username'];
+            $admin = $decoded_token['admin'];
+            $sql = "SELECT * FROM users WHERE username = ?";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $stmt->close();
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $password = $row['password'];
+                $ip = $_SERVER['REMOTE_ADDR'];
+                $user_agent = $_SERVER['HTTP_USER_AGENT'];
+                if (self::validate($username, $password, $ip, $user_agent, $admin, $token)) {
+
+                    $sql = "UPDATE users SET last_online = CURRENT_TIMESTAMP, last_ip = ?, last_user_agent = ? WHERE username = ?";
+                    $stmt = $this->conn->prepare($sql);
+                    $stmt->bind_param("sss", $ip, $user_agent, $username);
+                    $stmt->execute();
+
+                    return true;
+                }
+            }
+        } catch (Exception) {
+            return false;
         }
         return false;
     }
@@ -106,25 +139,22 @@ class Authentication
         return $result->num_rows > 0;
     }
 
-    private function generateToken(string $username, string $password, string $ip, string $user_agent): string
+    private function generateToken(string $username, string $password, string $ip, string $user_agent, bool $admin): string
     {
         $json = json_encode([
             'username' => $username,
             'password' => $password,
             'ip' => $ip,
-            'user_agent' => $user_agent
+            'user_agent' => $user_agent,
+            'admin' => $admin
         ]);
         $token = hash('sha256', $json);
-
-        $token = base64_encode(json_encode(["username" => $username, "token" => $token]));
-
-        return $token;
+        return base64_encode(json_encode(["username" => $username, "admin" => $admin, "token" => $token]));
     }
 
-    private function validate(string $username, string $password, string $ip, string $user_agent, string $token): bool
+    private function validate(string $username, string $password, string $ip, string $user_agent, bool $admin, string $token): bool
     {
-        $valid = hash_equals(self::generateToken($username, $password, $ip, $user_agent), $token);
-        return $valid;
+        return hash_equals(self::generateToken($username, $password, $ip, $user_agent, $admin), $token);
     }
 
     /**
